051c1dccce4133da550a043191a86923cee4abe1
Bryan Chung <chungbwc@gmail.com>
2024-02-19 22:35:23 +0800

    DepthAI exercises

---
+++ DepthAI02/DepthAI02.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import cv2
+import depthai as dai
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define source and output
+camRgb = pipeline.create(dai.node.ColorCamera)
+xoutVideo = pipeline.create(dai.node.XLinkOut)
+
+xoutVideo.setStreamName("video")
+
+# Properties
+camRgb.setBoardSocket(dai.CameraBoardSocket.CAM_A)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+camRgb.setVideoSize(1920, 1080)
+
+xoutVideo.input.setBlocking(False)
+xoutVideo.input.setQueueSize(1)
+
+# Linking
+camRgb.video.link(xoutVideo.input)
+
+# Connect to device and start pipeline
+with dai.Device(pipeline) as device:
+    video = device.getOutputQueue(name="video", maxSize=1, blocking=False)
+
+    running = True
+    while running:
+        videoIn = video.get()
+
+        cv2.imshow("video", videoIn.getCvFrame())
+
+        if cv2.waitKey(1) == ord('q'):
+            running = False
